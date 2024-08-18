c8df9e34b1705ccf0e867d95600a6068ff11e9c4
Veronika Sesay <veronika.sesay@gmail.com>
2024-08-18 20:54:28 +0200

    BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,23 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import './global.css';
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+createRoot(document.querySelector('#app')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<HomePage />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '3rem' }}>
+              <p>Tady není vůbec nic!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+);
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import './style.css';
 
 export const HomePage = () => {
@@ -11,28 +12,7 @@ export const HomePage = () => {
         </div>
       </header>
       <main>
-        <div className="main-content">
-          <h1>Gynekologická ordinace</h1>
-          <h1>MUDr. Jana Tůmová</h1>
-          <p>Připravujeme pro vás novou verzi webu, děkujeme za pochopení.</p>
-          <p className="bold">
-            Během letních prázdnin je v pondělí ordinační doba od 9:00 do 16:00.
-          </p>
-          <p className="bold">5. - 16.8. neordinujeme z důvodu dovolené:</p>
-          <p>5. - 9.8. zastupuje MUDr. Šmrhová</p>
-          <p>12. - 16. 8. zastupuje MUDr. Čmuchová</p>
-          <p>
-            Naše ordinace nyní používá pro zabezpečenou komunikaci s pacienty
-            virtuální sestru Emmy. Emmy nahrazuje email a do velké míry i
-            telefon. Usnadní vám přístup k péči i objednání a zároveň šetří
-            vzácný čas zdravotnickému personálu.
-          </p>
-          <p>Přímý vstup do naší ordinace je možný zde:</p>
-          <button>
-            {' '}
-            <a href="https://www.sestraemmy.cz/gyntum">Sestra Emmy</a>
-          </button>
-        </div>
+        <Outlet />
       </main>
       <footer></footer>
     </>
