5528d48299e051d5c8aa3f3b234989ddd07b1b46
Veronika Sesay <veronika.sesay@gmail.com>
2024-11-17 23:36:51 +0100

    Top row and navigation styled

--- src/pages/Navigation/index.jsx
+++ src/pages/Navigation/index.jsx
@@ -8,63 +8,58 @@ export const Navigation = () => {
 
   return (
     <>
-      <div className="top-row">
-        <div className="top-row-container">
-          <div className="top-row__item">
-            <img className="icon" src="../img/address.svg" alt="address icon" />
-            <p>Kpt. Jaroše 2876, 390&nbsp;03&nbsp;Tábor</p>
-          </div>
-          <div className="top-row__item">
-            <img className="icon" src="../img/phone.svg" alt="phone icon" />
-            <a href="tel:+420732726631">+420 732 726 631</a>
-          </div>
-          <div className="top-row__item">
-            <img className="icon" src="../img/email.svg" alt="email icon" />
-            <a href="mailto:info@gyntum.cz">info@gyntum.cz</a>
-          </div>
+      <div className="top-row container">
+        <div className="top-row__item">
+          <img className="icon" src="../img/address.svg" alt="address icon" />
+          <a href="">Kpt. Jaroše 2876, 390&nbsp;03&nbsp;Tábor</a>
+        </div>
+        <div className="top-row__item">
+          <img className="icon" src="../img/phone.svg" alt="phone icon" />
+          <a href="tel:+420732726631">+420 732 726 631</a>
+        </div>
+        <div className="top-row__item">
+          <img className="icon" src="../img/email.svg" alt="email icon" />
+          <a href="mailto:info@gyntum.cz">info@gyntum.cz</a>
         </div>
       </div>
-      <div className="menu-container">
-        <Link to="/home">
-          <video autoPlay muted>
-            <source src="../img/gyntum-logo.mp4" type="video/mp4" />
-          </video>
-        </Link>
-        <nav className="nav-container">
-          <Link to="/news">Aktuality</Link>
-          <Link to="/about">O nás</Link>
-          <Link to="/contacts">Kontakty</Link>
-          <button className="emmy">
-            {' '}
-            <Link to="/">Virtuální sestra Emmy</Link>
-          </button>
-        </nav>
-      </div>
+      <div className="menu-section">
+        <div className="menu container">
+          <Link to="/home">
+            <img className="logo" src="../img/logo-image.svg" alt="logo" />
+          </Link>
+          <nav className="nav">
+            <Link to="/news">Aktuality</Link>
+            <Link to="/about">O nás</Link>
+            <Link to="/contacts">Kontakty</Link>
+            <button className="emmy">
+              {' '}
+              <Link to="/">Virtuální sestra Emmy</Link>
+            </button>
+          </nav>
+        </div>
 
-      <div className="menu-mobile-container">
-        <Link to="/home">
-          <video src="../img/logo.gif" alt="logo" className="logo" />
-        </Link>
-        <button
-          onClick={() => setNavOpen(!navOpen)}
-          className={navOpen ? 'nav-btn--closed' : 'nav-btn'}
-        ></button>
-        <div
-          className={navOpen ? 'nav-mobile' : 'menu-mobile-container--closed'}
-        >
-          <Link onClick={closeNav} to="/news">
-            Aktuality
+        <div className="menu-mobile">
+          <Link to="/home">
+            <img className="logo" src="../img/logo-image.svg" alt="logo" />
           </Link>
-          <Link onClick={closeNav} to="/about">
-            O nás
-          </Link>
-          <Link onClick={closeNav} to="/contacts">
-            Kontakty
-          </Link>
-          <button className="emmy">
-            {' '}
-            <Link to="/">Virtuální sestra Emmy</Link>
-          </button>
+          <button
+            onClick={() => setNavOpen(!navOpen)}
+            className={navOpen ? 'nav-btn--closed' : 'nav-btn'}
+          ></button>
+          <div className={navOpen ? 'nav-mobile' : 'menu-mobile--closed'}>
+            <Link onClick={closeNav} to="/news">
+              Aktuality
+            </Link>
+            <Link onClick={closeNav} to="/about">
+              O nás
+            </Link>
+            <Link onClick={closeNav} to="/contacts">
+              Kontakty
+            </Link>
+            <Link className="emmy" onClick={closeNav} to="/">
+              Virtuální sestra Emmy
+            </Link>
+          </div>
         </div>
       </div>
     </>
