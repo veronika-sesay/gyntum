7be80575f6ec28d92a0d9d4ab798d593c719aae4
veronika-sesay <veronika.sesay@gmail.com>
2026-05-27 10:33:23 +0200

    Tsconfig update, news update

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -7,7 +7,7 @@ export const HomePage = () => {
     useEffect(() => {
         // Intersection Observer for scroll animations
         const observerOptions = {
-            threshold: 0.1,
+            threshold: 0.1, // Trigger when 10% of element is visible
             rootMargin: '0px 0px -50px 0px', // Trigger a bit before element fully enters viewport
         };
         const observer = new IntersectionObserver((entries) => {
